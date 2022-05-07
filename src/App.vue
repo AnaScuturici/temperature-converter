9d5348c3e5adfd7fd33b66e6c5bbebb8188da858
ana scuturici <anascuturici@gmail.com>
2022-05-07 22:18:06 +0900

    add input component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="container">
     <Header title="Temperature converter"/>
+    <Input title="temperature"/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Input from "./components/Input.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Input,
   }
 };
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>Enter {{title}}:</p>
+        <input type="number">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Input",
+    props: {
+        type: Number,
+        title: String,
+    },
+}
+</script>
+
+<style>
+</style>
