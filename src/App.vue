bd833dcc1b8f3306e6f496d1bb07005293ae2444
ana scuturici <anascuturici@gmail.com>
2022-05-07 22:59:54 +0900

    add selections component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div class="container">
     <Header title="Temperature converter"/>
     <Input title="temperature"/>
+    <Selections :options="['°C', '°F']"/>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Input from "./components/Input.vue";
+import Selections from "./components/Selections.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     Input,
+    Selections,
   }
 };
 </script>
---
+++ src/components/Selections.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>From:</p>
+        <select v-model="selected">
+            <option disabled value="">Please select one</option>
+            <option v-for="(option, index) in options" :key="index" :value="option">{{option}}</option>
+        </select>
+        <p>To:</p>
+        <select v-model="selected">
+            <option disabled value="">Please select one</option>
+            <option v-for="(option, index) in options" :key="index" :value="option">{{option}}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Selections",
+    props: {
+        options: String,
+    },
+}
+</script>
+
+<style>
+</style>
