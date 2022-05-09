6c5b7c188fda43689eab6c9276752e689afb9112
ana scuturici <anascuturici@gmail.com>
2022-05-09 21:30:26 +0900

    add Toggle component

---
+++ src/components/Toggle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <i
+      @click="toggle"
+      :class="[isActive ? 'fa-toggle-off' : 'fa-toggle-on', 'fa-solid']"
+    ></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toggle",
+  data() {
+    return {
+      isActive: false,
+    };
+  },
+  props: {
+    mode: {
+      type: String,
+      default: "dark",
+    },
+  },
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive;
+      this.$emit('toggle');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.fa-solid {
+  font-size: 30px;
+}
+</style>
